f6cea6e1b79058575b8445fbf62a6203ed8c8b58
yuma14 <yuma140902@gmail.com>
2024-10-09 00:59:28 +0900

    feat: create window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,77 @@
-fn main() {
+use windows::Win32::{
+    Foundation::{HMODULE, HWND, LPARAM, LRESULT, WPARAM},
+    Graphics::Gdi::ValidateRect,
+    System::LibraryLoader::GetModuleHandleW,
+    UI::WindowsAndMessaging::{
+        CreateWindowExW, DefWindowProcW, DispatchMessageW, GetMessageW, LoadCursorW,
+        PostQuitMessage, RegisterClassW, CS_HREDRAW, CS_VREDRAW, CW_USEDEFAULT, IDC_ARROW, MSG,
+        WINDOW_EX_STYLE, WM_DESTROY, WM_PAINT, WNDCLASSW, WS_OVERLAPPEDWINDOW, WS_VISIBLE,
+    },
+};
+use windows_core::{w, PCWSTR};
+
+fn main() -> anyhow::Result<()> {
     println!("Hello, world!");
+
+    unsafe {
+        let instance: HMODULE = GetModuleHandleW(None)?;
+        let window_class: PCWSTR = w!("RvViewWindow");
+
+        let wc = WNDCLASSW {
+            style: CS_HREDRAW | CS_VREDRAW,
+            lpfnWndProc: Some(wndproc),
+            hInstance: instance.into(),
+            hCursor: LoadCursorW(None, IDC_ARROW)?,
+            lpszClassName: window_class,
+            ..Default::default()
+        };
+
+        let atom = RegisterClassW(&wc);
+        if atom == 0 {
+            return Err(windows_core::Error::from_win32().into());
+        }
+
+        CreateWindowExW(
+            WINDOW_EX_STYLE::default(),
+            window_class,
+            w!("RvView"),
+            WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            None,
+            None,
+            instance,
+            None,
+        )?;
+
+        let mut message = MSG::default();
+
+        while GetMessageW(&mut message, None, 0, 0).into() {
+            DispatchMessageW(&message);
+        }
+    }
+
+    println!("App exit");
+
+    Ok(())
+}
+
+extern "system" fn wndproc(window: HWND, message: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+    unsafe {
+        match message {
+            WM_PAINT => {
+                println!("WM_PAINT");
+                _ = ValidateRect(window, None);
+                LRESULT(0)
+            }
+            WM_DESTROY => {
+                println!("WM_DESTROY");
+                PostQuitMessage(0);
+                LRESULT(0)
+            }
+            _ => DefWindowProcW(window, message, wparam, lparam),
+        }
+    }
 }
